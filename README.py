5c1c464b7a3188e8b5144617e358114ff5e69ce5
davinasi <144905548+davinasi@users.noreply.github.com>
2023-10-16 02:29:58 +0800

    Update README.py

--- README.py
+++ README.py
@@ -1,5 +1,4 @@
 # postest-2
-
 import getpass
 # PROGRAM LOGIN SEDERHANA
 print ('__________________')
@@ -49,13 +48,6 @@ def buyer_menu(store):
     print ("Silahkan hubungi sales untuk promo menarik")
     print ("Mr. Pandu : 085752358477")
     print ("atau kunjungi website kami www.hondanusantara.com")
-            
-print("\nMenu:")
-print("1. NEW HONDA HRV 1.5 S CVT")
-print("2. NEW HONDA CRV 2.0 i-VTEC")
-print("3. NEW HONDA CIVIC 1.5 CVT Turbo RS1")
-input('pilih kendaraan yang ingin dibeli: ')
-print ("pesanan anda telah kami proses")
 
 
 
@@ -70,31 +62,55 @@ def admin_menu(store):
         print("1. Add Product")
         print("2. Delete Product")
         print("3. View Products")
-        print("4. Exit")
+        print("4. replace PRICE AND STOCK")
+        print("5. Exit")
+
         choice = int(input("Enter your choice: "))
 
         if choice == 1:
             name = input("Enter product name: ")
             price = float(input("Enter product price: "))
-            stock = (input("Enter product stock: "))
+            stock = int(input("Enter product stock: "))
             product = Product(name, price, stock)
             store.add_product(product)
             print("Product added successfully.")
 
         elif choice == 2:
-            delete_data= input("Enter the data to delete rows: ")
-            rows_to_delete = [row for row in data1 if row["data1"] == delete_data1]
-            for row in rows_to_delete:
-                table.del_row(table.get_string(fields=["data"]).split('\n').index(row["data"]) + 1)
+            name_to_delete = input("Enter the product name to delete: ")
+            product_found = False
 
-           
+            # Find and delete the product by name
+            for product in store.products:
+                if product.name == name_to_delete:
+                    store.products.remove(product)
+                    print("Product deleted successfully.")
+                    product_found = True
+                    break
             
+
 
         elif choice == 3:
             store.view_products()
 
         elif choice == 4:
+            name_to_replace = input("Enter the product name to replace: ")
+            new_price = float(input("Enter the new product price: "))
+            new_stock = int(input("Enter the new product stock: "))
+            product_found = False
+            for product in store.products:
+                if product.name == name_to_replace:
+                    product.price = new_price
+                    product.stock = new_stock
+                    print("Product information replaced successfully.")
+                    product_found = True
+                    break
+
+            if not product_found:
+                print("Product not found.")
+
+        elif choice == 5:
             print("admin logged out.")
+            break
 
         else:
             print("Invalid choice. Try again.")
@@ -103,9 +119,9 @@ if __name__ == "__main__":
     store = Store()
 
     # Dummy products for demonstration
-    data1 = store.add_product(Product("NEW HONDA HRV 1.5 S CVT", 375.900 , 23))
-    data2 = store.add_product(Product("NEW HONDA CRV 2.0 i-VTEC", 525.300, 190))
-    data3 = store.add_product(Product("NEW HONDA CIVIC 1.5 CVT Turbo RS", 601.400, 67))
+    store.add_product(Product("NEW HONDA HRV 1.5 S CVT", 375.900 , 23))
+    store.add_product(Product("NEW HONDA CRV 2.0 i-VTEC", 525.300, 190))
+    store.add_product(Product("NEW HONDA CIVIC 1.5 CVT Turbo RS", 601.400, 67))
 
     print("Choose your role:")
     print("1. Customer")
